72621b59006ca773758c601d730d4d0415b7039e
junggayeong <152073691+junggayeong@users.noreply.github.com>
2026-05-06 15:11:59 +0900

    Chore: user엔티티 제약조건 및 변수 추가
    - 비밀번호, 이메일 추가
    - 각 변수에 제약조건 추가

--- bugbug/src/main/java/com/bug/catcher/domain/entity/User.java
+++ bugbug/src/main/java/com/bug/catcher/domain/entity/User.java
@@ -16,8 +16,16 @@ public class User {
     @Column(name = "user_id")
     private Long id;
 
+    @Column(nullable = false, length = 50)
     private String nickname;
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+    @Column(nullable = false, length = 255)
+    private String password;
+
     private String phoneNumber;
     private String address;
+
+    @Column(nullable = false, length = 20)
     private String role;
 }
