2e8a554071bfb44dd635b290e2f0a6a05ff0bf2f
jiho422 <jhnam0422@naver.com>
2026-05-08 17:55:02 +0900

    원복

--- bugbug/src/main/java/com/bug/catcher/domain/request/repository/RequestImageRepository.java
+++ bugbug/src/main/java/com/bug/catcher/domain/request/repository/RequestImageRepository.java
@@ -1,11 +1,4 @@
 package com.bug.catcher.domain.request.repository;
 
-import com.bug.catcher.domain.entity.RequestImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface RequestImageRepository extends JpaRepository<RequestImage, Long> {
-
-    List<RequestImage> findByRequestId(Long requestId);
+public interface RequestImageRepository {
 }
