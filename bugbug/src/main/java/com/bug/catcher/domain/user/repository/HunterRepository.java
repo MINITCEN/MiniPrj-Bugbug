5f197342fdada2c241c4b3b4467cd003f7ae94f7
junggayeong <152073691+junggayeong@users.noreply.github.com>
2026-05-11 10:56:48 +0900

    remove: 유저와 헌터 도메인 헌터레포파일 중복 문제 해결

--- bugbug/src/main/java/com/bug/catcher/domain/user/repository/HunterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bug.catcher.domain.user.repository;
-
-import com.bug.catcher.domain.entity.Hunter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HunterRepository extends JpaRepository<Hunter, Long> {
-}
