77e9e1db93b00557b58f48f571541dda64a75d21
junggayeong <152073691+junggayeong@users.noreply.github.com>
2026-05-11 20:47:35 +0900

    feat: 헌터 목록조회 및 찜하기 기능 추가

--- bugbug/src/main/java/com/bug/catcher/domain/hunter/controller/HunterController.java
+++ bugbug/src/main/java/com/bug/catcher/domain/hunter/controller/HunterController.java
@@ -1,10 +1,17 @@
 package com.bug.catcher.domain.hunter.controller;
 
+import com.bug.catcher.domain.entity.User;
 import com.bug.catcher.domain.hunter.dto.HunterProfileResponseDto;
 import com.bug.catcher.domain.hunter.service.HunterService;
+import com.bug.catcher.global.auth.SessionConst;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.domain.Sort;
+import com.bug.catcher.domain.hunter.dto.HunterListResponseDto;
 
 @RestController
 @RequestMapping("/api/hunters")
@@ -17,6 +24,24 @@ public class HunterController {
     @GetMapping("/{hunterId}/profile")
     public ResponseEntity<HunterProfileResponseDto> getHunterProfile(@PathVariable Long hunterId) {
         HunterProfileResponseDto response = hunterService.getHunterProfile(hunterId);
+        return ResponseEntity.ok(response);
+    }
+    // 헌터 찜하기 / 찜 취소 토글
+    @PostMapping("/{hunterId}/bookmarks")
+    public ResponseEntity<String> toggleSavedHunter(
+            @PathVariable Long hunterId,
+            @SessionAttribute(SessionConst.LOGIN_USER) User loginUser) {
+
+        String message = hunterService.toggleSavedHunter(loginUser.getId(), hunterId);
+        return ResponseEntity.ok(message);
+    }
+    // 전체 헌터 목록 조회 API
+    @GetMapping
+    public ResponseEntity<Page<HunterListResponseDto>> getHunters(
+            @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+            @SessionAttribute(SessionConst.LOGIN_USER) User loginUser) {
+
+        Page<HunterListResponseDto> response = hunterService.getHunterList(loginUser.getId(), pageable);
         return ResponseEntity.ok(response);
     }
 }
